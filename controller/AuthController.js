49f433ecf6940475d9566913bb779307437b9839
Karan Raj <karanraj110505@gmail.com>
2025-07-29 22:26:31 +0530

    feat(auth): implement logout protection middleware and AuthContext
    - Added middleware to protect routes like /roadmap, /dashboard, and /profile
    - Created AuthContext with isLoggedIn and setIsLoggedIn state to manage auth globally

--- controller/AuthController.js
+++ controller/AuthController.js
@@ -95,3 +95,19 @@ export const login = async (req, res) => {
   }
 }
 
+
+export const logout = async(req,res)=>{
+  try {
+    res.clearCookie('token',{
+      httpOnly:true,
+      secure:false,
+      sameSite:'lax',
+      path:'/'
+    })
+
+    return res.status(200).json({message:'Logged out successfully'})
+  } catch (error) {
+    console.error('Logout error:', error)
+    return res.status(500).json({ message: 'Something went wrong during logout' })
+  }
+}
--- routes/authRouter.js
+++ routes/authRouter.js
@@ -1,10 +1,13 @@
 import { Router } from "express";import express from 'express';
 import { signupValidation,loginValidation } from "../Middlewares/AuthValidation.js";
 import { login, signup } from "../controller/AuthController.js";
+import ensureAuthenticated from "../Middlewares/Auth.js";
+import { logout } from "../controller/AuthController.js";
 const router = express.Router();
 
 router.post('/Signup',signupValidation,signup);
 router.post('/login',loginValidation,login);
+router.post('/logout',ensureAuthenticated,logout)
 
 export default router;
 
